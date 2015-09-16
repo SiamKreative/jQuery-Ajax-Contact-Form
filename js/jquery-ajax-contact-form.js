6e511e87b16654eba3b7203860305f1f4085b6a8
Julien Vernet <julien@vernet.me>
2015-09-16 12:01:38 +0700

    Initial commit

---
+++ js/jquery-ajax-contact-form.js
@@ -0,0 +1,104 @@
+jQuery(document).ready(function ($) {
+
+	$('form[data-form-ajax]').on('submit', function (event) {
+
+		/**
+		 * Cache selectors in variables
+		 */
+		var $form = $(this);
+		var $submitBtn = $('[type="submit"]', $form);
+		var $redirect = $form.attr('data-form-redirect');
+		var $msgSuccess = $form.attr('data-form-success');
+		var $emailTo = $form.attr('action').match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+		var $geoData = '';
+
+		// Avoid multiple form submissions + Update the UI
+		$submitBtn.prop('disabled', false).text('Sending...');
+
+		/**
+		 * Process the form using AJAX
+		 * http://api.jquery.com/jquery.ajax/
+		 */
+		function formAjax() {
+
+			$.ajax({
+
+				type: $form.attr('method'),
+				url: $form.attr('action'),
+				data: $form.serialize(),
+				dataType: 'json',
+				encode: true
+
+			})
+
+			// Using the done promise callback
+			.done(function (data) {
+
+				// Handle errors and validation messages
+				if (!data.success) {
+
+					// Show the error message
+					$form.append('<div class="alert alert-danger form-status">Oops something went wrong. Email us at <a href="mailto:' + $emailTo + '">' + $emailTo + '</a>.</div>');
+
+				} else {
+
+					// Restore submit button
+					$submitBtn.prop('disabled', false).text('Email sent!');
+
+					// Redirect the user to thanks page after form submission (if there is the attribute data-form-redirect)
+					if (typeof $redirect !== typeof undefined && $redirect !== false) {
+
+						window.location = $redirect;
+
+					}
+
+					// Show the success message
+					else {
+
+						if (typeof $msgSuccess !== typeof undefined && $msgSuccess !== false) {
+							$form.append('<div class="alert alert-success form-status">' + $msgSuccess + '</div>');
+						} else {
+							$form.append('<div class="alert alert-success form-status">' + data.success + '</div>');
+						}
+
+					}
+
+				}
+
+			});
+		}
+
+		/**
+		 * Add geolocation data
+		 * http://ip-api.com/docs/api:json
+		 */
+		$.getJSON('//ip-api.com/json', function (json, textStatus) {
+
+			if (json.status == 'success') {
+
+				// Loop through the JSON object
+				$.each(json, function (index, val) {
+					$geoData += '<input type="hidden" name="geodata_' + index + '" value="' + val + '">';
+				});
+
+				// Append geodata only once
+				$form.append($geoData);
+
+				// Submit the form
+				formAjax();
+
+			}
+
+			// Something went wrong with the Geolocation. Lets submit the form without any geodata
+			else {
+
+				formAjax();
+
+			}
+		});
+
+		// Stop the form from submitting the normal way and refreshing the page
+		event.preventDefault();
+	});
+
+});
